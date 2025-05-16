5e265343722d94cdfbe3358a192b07831853f2ad
martHaza <martazapacka17@gmail.com>
2025-05-16 17:27:07 +0300

    Izveidoti faili - demoqa, basePage un selectablePage

---
+++ cypress/e2e/demoqa.cy.js
@@ -0,0 +1,33 @@
+const { SelectablePage } = require("../pageObjects/SelectablePage");
+
+describe('demoqa scenarios', () => {
+   beforeEach(() => {
+     cy.visit('https://demoqa.com/selectable');
+ });
+  it('grid scenario', () => {
+    // a. Open https://demoqa.com/selectable
+    SelectablePageelectablePage.visit();
+
+    // b. Click “Grid”
+    SelectablePage.gridButton.click();
+
+  // c. Click - “Two”, “Four”, “Six”, “Eight”
+    SelectablePage.numberButton("Two").click();
+    SelectablePage.numberButton("Four").click();
+    SelectablePage.numberButton("Six").click();
+    SelectablePage.numberButton("Eight").click();
+
+  // d. Validate that “Two”, “Four”, “Six”, “Eight” are highlighted
+    SelectablePage.checkIfHighlighted("Two");
+    SelectablePage.checkIfHighlighted("Four");
+    SelectablePage.checkIfHighlighted("Six");
+    SelectablePage.checkIfHighlighted("Eight");
+
+  // e. Validate that “One”, “Three”, “Five”, “Seven”, “Nine” are not highlighted
+    SelectablePage.checkIfNotHighlighted("One");
+    SelectablePage.checkIfNotHighlighted("Three");
+    SelectablePage.checkIfNotHighlighted("Five");
+    SelectablePage.checkIfNotHighlighted("Seven");
+    SelectablePage.checkIfNotHighlighted("Nine");
+  })
+})

f677d64af6e4a2527d8213db689e87e43030ee6d
martHaza <martazapacka17@gmail.com>
2025-05-16 17:39:03 +0300

    number

--- pageObjects/selectablePage.js
+++ pageObjects/selectablePage.js
@@ -6,13 +6,13 @@ export class SelectablePage extends BasePage{
     static get gridButton(){
         return cy.get("a#demo-tab-grid");
     }
-    static numberButton(nmbr){
-        return cy.get("div#gridContainer").contains(nmbr);
+    static numberButton(number){
+        return cy.get("div#gridContainer").contains(number);
     }
-    static checkIfHighlighted(nmbr){
-        return cy.get('[class="list-group-item active list-group-item-action"]').should("contain", nmbr);
+    static checkIfHighlighted(number){
+        return cy.get('[class="list-group-item active list-group-item-action"]').should("contain", number);
     }
-    static checkIfNotHighlighted(nmbr){
-        return cy.get('[class="list-group-item list-group-item-action"]').should("contain", nmbr);
+    static checkIfNotHighlighted(number){
+        return cy.get('[class="list-group-item list-group-item-action"]').should("contain", number);
     }
   }
